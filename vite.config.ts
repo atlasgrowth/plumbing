b4b5f52cd3d5487a6a78deb75960e248dff5db87
atlasgrowth <nicholas@atlasgrowth.ai>
2025-02-19 11:15:22 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [
     react(),
@@ -28,6 +29,8 @@ export default defineConfig({
     },
   },
   root: path.resolve(__dirname, "client"),
+  // Add base path for GitHub Pages
+  base: "/plumbing/",
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
